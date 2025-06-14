fab6942cf7b642e140e6fe0a3980d5a6c9053c56
jeroenjota <jeroen@jota.nl>
2025-06-14 13:25:26 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:jeroenjota/jeroenjota.github.io.git mamain
+# git push -f git@github.com:jeroenjota/jeroenjota.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jeroenjota/kraakscore.git main:gh-pages
+git push -f git@github.com:jeroenjota/kraakscore.git master:gh-pages
 
 cd -
