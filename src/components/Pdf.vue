c4a34c247c9789780659a03f932f1998d0d23bf4
jeroenjota <jeroen@jota.nl>
2025-06-16 21:10:48 +0200

    center tables

--- src/components/Pdf.vue
+++ src/components/Pdf.vue
@@ -27,40 +27,57 @@ function getMatchesFromStorage() {
 function exportPdf() {
   const matches = getMatchesFromStorage();
   const doc = new jsPDF();
+  const pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
   const head = ["Ronde", "Tafel", "Match", "Uitslag", "tegen"];
   doc.setFontSize(24);
   const datum = new Date().toLocaleDateString("nl-NL");
-  doc.text("Overzicht kraaktoernooi van " + datum, 14, 20);
+  doc.text("Overzicht kraaktoernooi van " + datum, pageWidth/2, 20, { align:'center'});
+  doc.line(14, 22, pageWidth-14, 22);
   doc.setFontSize(12);
   let yPos = 20;
   let table = new Array();
   if (matches.length === 0) {
-    doc.text("Geen data gevonden in localStorage.", 14, yPos+=8);
+    doc.text("Geen data gevonden in localStorage.", 14, (yPos += 8));
   } else {
+    doc.setFontSize(18);
+    doc.text("Stand ", pageWidth/2, (yPos += 12), {align:'center'});
+    autoTable(doc, {
+      theme: "striped",
+      startY: (yPos += 2),
+      html: "#standTabel",
+      columnStyles: { 3: { halign: "center" } },
+      tableWidth: 100,
+      margin: { left:(pageWidth- 100)/2 },
+    });
+    yPos += doc.lastAutoTable.finalY - 26;
+    doc.line(14, yPos-6, pageWidth-14, yPos-6);
+    doc.text("Ronde uitslagen", pageWidth/2, yPos, {align:'center'});
     matches.forEach((m, index) => {
       // doc.text(`Ronde ${index+1}`, 14, yPos+=8)
-      if(m[0].homeScore || m[0].awayScore){
-
-        table = []
+      if (m[0].homeScore || m[0].awayScore) {
+        table = [];
         m.forEach((t, idx) => {
-          if(t.homeScore || t.awayScore){
-            let obj = [index+1, idx + 1, t.home + " vs " + t.away, String(t.homeScore) , String(t.awayScore)]
-            table.push(obj)
+          if (t.homeScore || t.awayScore) {
+            let obj = [index + 1, idx + 1, t.home + " vs " + t.away, String(t.homeScore), String(t.awayScore)];
+            table.push(obj);
           }
         });
-        console.log("Table:", table)
-        autoTable(doc, {theme:'grid',
-          head: [head], 
-          startY: yPos+=1, 
-          columnStyles: {3: {halign: 'center'}, 4:{halign:'center'}},
-          body: table });
-        yPos +=30;
+        autoTable(doc, {
+          theme: "grid",
+          head: [head],
+
+          startY: (yPos += 2),
+          columnStyles: { 
+            0: { row}
+            3: { halign: "center" }, 
+            4: { halign: "center" } },
+          tableWidth: 140,
+          body: table,
+          margin: { left:(pageWidth- 140)/2 },
+        });
+        yPos += 30;
       }
     });
-
-    doc.setFontSize(24);
-    doc.text("Stand ", 14, (yPos += 10));
-    autoTable(doc, {theme:'plain', startY: (yPos += 4), html: "#standTabel" });
   }
 
   doc.save("kraaktoernooi.pdf");
