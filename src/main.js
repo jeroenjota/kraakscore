0a02881dac2a885f92703c2376d920215399f5b6
jeroenjota <jeroen@jota.nl>
2025-06-30 12:56:00 +0200

    added vue3-touch-events

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "/styles.css";
-
+// import Vue3TouchEvents from 'vue3-touch-events';
+// App.use(Vue3TouchEvents, {disableClick: false})
 createApp(App).mount('#app')
