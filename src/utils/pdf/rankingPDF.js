2b2248472e14142c7c344eaf1e81ddd7729083b5
jeroenjota <jeroen@jota.nl>
2026-06-23 17:31:43 +0200

    wip

--- src/utils/pdf/rankingPDF.js
+++ src/utils/pdf/rankingPDF.js
@@ -133,12 +133,17 @@ export function rankingPDF(doc, ranking, toernooien, datum) {
         const colIndex = data.column.index;
         const rowIndex = data.row.index;
 
-        // Eerste rij altijd vet (plaats 1)
-        const highScore = ranking[0]?.totaal; 
-        const currentTotaal = ranking[rowIndex]?.totaal;
-        const prevTotaal = highScore
+        const current = ranking[rowIndex];
+        const above = ranking[rowIndex - 1];
+        const sameAsAbove =
+          rowIndex > 0 &&
+          current?.totaal === above?.totaal &&
+          current?.average === above?.average &&
+          current?.count12 === above?.count12 &&
+          current?.count9 === above?.count9 &&
+          current?.count6 === above?.count6 &&
+          current?.count3 === above?.count3;
         const isTopRow = rowIndex === 0;
-        const sameAsAbove = currentTotaal === prevTotaal;
 
         const isNameCol = colIndex === 1;
         const isTotaalCol = colIndex === data.table.columns.length - 1;
